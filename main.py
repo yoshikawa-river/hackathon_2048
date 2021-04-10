15a6628b8484e36769797aefbf8047494c8091fc
吉川佑太 <ohyu0@MacBook-Pro-2.local>
2021-04-10 14:49:23 +0900

    足し算導入

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import math
+import random
 
 canvas = None
 
@@ -11,6 +12,9 @@ NUMBER = 4
 LENGTH = SQUARE_LENGTH * NUMBER + BORDER_WIDTH * NUMBER
 CELL_COLOR = '#cbbeb5'
 BORDER_COLOR = '#b2a698'
+CELL_MAX = 3
+
+cells = []
 
 def set_field():
   canvas.create_rectangle(POSITION["x"], POSITION["y"], LENGTH + POSITION["x"], LENGTH + POSITION["y"], fill='#cbbeb5', width=BORDER_WIDTH, outline=BORDER_COLOR)
@@ -31,20 +35,100 @@ def create_canvas():
   return root, canvas
 
 def set_number(num, x, y):
-  center_x = POSITION["x"] + BORDER_WIDTH * x + BORDER_WIDTH / 2 + SQUARE_LENGTH * x + SQUARE_LENGTH / 2
-  center_y = POSITION["y"] + BORDER_WIDTH * y + BORDER_WIDTH / 2 + SQUARE_LENGTH * y + SQUARE_LENGTH / 2
-  canvas.create_rectangle(center_x - SQUARE_LENGTH / 2, center_y - SQUARE_LENGTH / 2, center_x + SQUARE_LENGTH / 2, center_y + SQUARE_LENGTH / 2, fill=CELL_COLOR, width=0)
-  canvas.create_text(center_x, center_y, text=num, justify="center", font=("", 70), tag="count_text")
+    center_x = POSITION["x"] + BORDER_WIDTH * x + BORDER_WIDTH / 2 + SQUARE_LENGTH * x + SQUARE_LENGTH / 2
+    center_y = POSITION["y"] + BORDER_WIDTH * y + BORDER_WIDTH / 2 + SQUARE_LENGTH * y + SQUARE_LENGTH / 2
+    canvas.create_rectangle(center_x - SQUARE_LENGTH / 2, center_y - SQUARE_LENGTH / 2, center_x + SQUARE_LENGTH / 2, center_y + SQUARE_LENGTH / 2, fill=CELL_COLOR, width=0)
+    if num != 0:
+      canvas.create_text(center_x, center_y, text=num, justify="center", font=("", 70), tag="count_text")
 
 def operate(event):
-  print(event.keysym)
+  if event.keysym == 'Up' or event.keysym == 'Down':#上下関数呼び出し
+    move_vertical(event.keysym)
+  else:#左右関数呼び出し
+    move_side(event.keysym)
+  new_field()
+
+class Cell():
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.count = 0
+
+def change_number(x, y):
+  number = y * NUMBER + x 
+  return number
+
+def move_vertical(key):#上下関数
+  if key == 'Up':
+    new_y = 0
+  else:
+    new_y = CELL_MAX
+  for cell in cells:
+    if cell.count != 0:
+      if cell.y != new_y:
+        new_num = change_number(cell.x, new_y)
+        cells[new_num].count = cell.count
+        cell.count = 0
+
+def move_side(key):#左右関数
+  if key == 'Left':
+    new_x = 0
+  else:
+    new_x = CELL_MAX
+  for cell in cells:
+    if cell.count != 0:
+      if cell.x != new_x:
+        new_num = change_number(new_x, cell.y)
+        cells[new_num].count = cell.count
+        cell.count = 0
+
+# def is_vertical(x, y):
+#   number = change_number(x, y)
+#   print(number)
+#   if cells[number].count == 0:
+#     return True
+#   elif number > 15:
+#     return False
+#   else:
+#     return False
+
+# def vertical_bottom(x, y, old_num):
+#   i = y
+#   print(x, i)
+#   while not is_vertical(x, i):
+#     i += 1
+#   new_num = change_number(x, i)
+#   # print(new_num)
+#   if cells[new_num].count == cells[old_num].count:
+#     cells[new_num].count *= 2
+#     cells[old_num].count = 0
+
+# old_num = change_number(x, y)
+  # deploy_count(new_count, old_count)
+  
+def new_field():#再描画
+  for cell in cells:
+    set_number(cell.count, cell.x, cell.y)
+
+def create_cells():
+  global cells
+  for y in range(NUMBER):
+    for x in range(NUMBER):
+      cells.append(Cell(x, y))
+      # print(x, y)
+
+def test():
+  cells[0].count = 2
+  cells[10].count = 2
+  set_number(cells[0].count, cells[0].x, cells[0].y)
+  set_number(cells[10].count, cells[10].x, cells[10].y)
 
 def play():
   global canvas
   root, canvas = create_canvas()
   set_field()
-  set_number("2", 0, 3)
-  set_number("4", 2, 1)
+  create_cells()
+  test()
   root.bind("<Key>", lambda event: operate(event))
   root.mainloop()
 
